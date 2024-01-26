1bafe01a883cb4ccfd22b1e2fdce1298bd7ff7b1
José Riobueno <jrrv99@gmail.com>
2024-01-26 17:36:01 -0400

    refactor: implement the way used in class to find the gcd

--- LabSem2/SonNumerosCoprimos.py
+++ LabSem2/SonNumerosCoprimos.py
@@ -3,11 +3,12 @@ SECOND_NUM_INPUT_MESSAGE: str = "Indique el segundo número: \n"
 NEGATIVE_NUMS_ERROR_MESSAGE: str = "Ambos números deben ser positivos."
 
 
-def mcd(num1: int, num2: int) -> int:
-	while num2 != 0:
-		temp = num2
-		num2 = num1 % num2
-		num1 = temp
+def gcd(num1: int, num2: int) -> int:
+	while num1 != num2:
+		if num1 > num2:
+			num1 -= num2
+		else:
+			num2 -= num1
 
 	return num1
 
@@ -20,7 +21,7 @@ def main() -> None:
 		print(NEGATIVE_NUMS_ERROR_MESSAGE)
 		return
 	
-	print(mcd(num1, num2) == 1)
+	print(gcd(num1, num2) == 1)
 
 
 if __name__ == "__main__":

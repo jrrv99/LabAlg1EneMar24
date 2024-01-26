1afee9c89afe30bb6fa97f3e0a2d49c50987657e
José Riobueno <jrrv99@gmail.com>
2024-01-26 13:07:13 -0400

    test(LabSem2): improve tests outputs

--- LabSem2/light_tests.py
+++ LabSem2/light_tests.py
@@ -18,24 +18,24 @@ def test_problem_1() -> float:
         ("-1", "El número debe ser no negativo.")
     ]
 
-    for test in test_cases:
+    for input_value, spected_output in test_cases:
         process = subprocess.run(
             ["python3", "VolumenAreaCubo.py"],
-            input=test[0],
+            input=input_value,
             text=True,
             capture_output=True
         )
 
         output: str = process.stdout.split("\n")[-2].strip()
-        test_result: bool = output == test[1]
+        test_result: bool = output == spected_output
 
         if test_result:
             print(
-                f"\033[1;36m**\033[0m Caso de prueba correcto: {test[0]} -> {test[1]}")
+                f"\033[1;36m**\033[0m Caso de prueba correcto: {input_value} -> {spected_output}")
             result += 1
         else:
             print(
-                f"\033[1;31m**\033[0m Caso de prueba incorrecto: {test[0]} -> {test[1]}")
+                f"\033[1;31m**\033[0m Caso de prueba incorrecto: {input_value} -> {spected_output}")
             print(f"Output: {output}")
             if process.stderr:
                 print(f"Error: {process.stderr.strip()}")
@@ -61,26 +61,27 @@ def test_problem_2() -> float:
     test_cases: List[Tuple[str, str]] = [
         ("1\n0\n0", "True"),
         ("-1\n0\n0", "El radio del círculo debe ser no negativo."),
+        ("10\n10\n0", "True"),
     ]
 
-    for test in test_cases:
+    for input_value, spected_output in test_cases:
         process = subprocess.run(
             ["python3", "DentroDelCirculo.py"],
-            input=test[0],
+            input=input_value,
             text=True,
             capture_output=True
         )
 
         output: str = process.stdout.split("\n")[-2].strip()
-        test_result: bool = output == test[1]
+        test_result: bool = output == spected_output
 
         if test_result:
             print(
-                f"\033[1;36m**\033[0m Caso de prueba correcto: {test[0]} -> {test[1]}")
+                f"\033[1;36m**\033[0m Caso de prueba correcto: {', '.join(input_value.splitlines())} -> {spected_output}")
             result += 1
         else:
             print(
-                f"\033[1;31m**\033[0m Caso de prueba incorrecto: {test[0]} -> {test[1]}")
+                f"\033[1;31m**\033[0m Caso de prueba incorrecto: {input_value} -> {spected_output}")
             print(f"Output: {output}")
             if process.stderr:
                 print(f"Error: {process.stderr.strip()}")
@@ -108,24 +109,24 @@ def test_problem_3() -> float:
         ("0", "El año debe estar en el rango [1900, 2200].")
     ]
 
-    for test in test_cases:
+    for input_value, spected_output in test_cases:
         process = subprocess.run(
             ["python3", "EsBisiesto.py"],
-            input=test[0],
+            input=input_value,
             text=True,
             capture_output=True
         )
 
         output: str = process.stdout.split("\n")[-2].strip()
-        test_result: bool = output == test[1]
+        test_result: bool = output == spected_output
 
         if test_result:
             print(
-                f"\033[1;36m**\033[0m Caso de prueba correcto: {test[0]} -> {test[1]}")
+                f"\033[1;36m**\033[0m Caso de prueba correcto: {input_value} -> {spected_output}")
             result += 1
         else:
             print(
-                f"\033[1;31m**\033[0m Caso de prueba incorrecto: {test[0]} -> {test[1]}")
+                f"\033[1;31m**\033[0m Caso de prueba incorrecto: {input_value} -> {spected_output}")
             print(f"Output: {output}")
             if process.stderr:
                 print(f"Error: {process.stderr.strip()}")
@@ -153,24 +154,24 @@ def test_problem_4() -> float:
         ("-1", "El número debe ser no negativo."),
     ]
 
-    for test in test_cases:
+    for input_value, spected_output in test_cases:
         process = subprocess.run(
             ["python3", "EsCuadradoPerfecto.py"],
-            input=test[0],
+            input=input_value,
             text=True,
             capture_output=True
         )
 
         output: str = process.stdout.split("\n")[-2].strip()
-        test_result: bool = output == test[1]
+        test_result: bool = output == spected_output
 
         if test_result:
             print(
-                f"\033[1;36m**\033[0m Caso de prueba correcto: {test[0]} -> {test[1]}")
+                f"\033[1;36m**\033[0m Caso de prueba correcto: {input_value} -> {spected_output}")
             result += 1
         else:
             print(
-                f"\033[1;31m**\033[0m Caso de prueba incorrecto: {test[0]} -> {test[1]}")
+                f"\033[1;31m**\033[0m Caso de prueba incorrecto: {input_value} -> {spected_output}")
             print(f"Output: {output}")
             if process.stderr:
                 print(f"Error: {process.stderr.strip()}")
@@ -198,24 +199,24 @@ def test_problem_5() -> float:
         ("0\n0", "Ambos números deben ser positivos."),
     ]
 
-    for test in test_cases:
+    for input_value, spected_output in test_cases:
         process = subprocess.run(
             ["python3", "SonNumerosCoprimos.py"],
-            input=test[0],
+            input=input_value,
             text=True,
             capture_output=True
         )
 
         output: str = process.stdout.split("\n")[-2].strip()
-        test_result: bool = output == test[1]
+        test_result: bool = output == spected_output
 
         if test_result:
             print(
-                f"\033[1;36m**\033[0m Caso de prueba correcto: {test[0]} -> {test[1]}")
+                f"\033[1;36m**\033[0m Caso de prueba correcto: {input_value} -> {spected_output}")
             result += 1
         else:
             print(
-                f"\033[1;31m**\033[0m Caso de prueba incorrecto: {test[0]} -> {test[1]}")
+                f"\033[1;31m**\033[0m Caso de prueba incorrecto: {input_value} -> {spected_output}")
             print(f"Output: {output}")
             if process.stderr:
                 print(f"Error: {process.stderr.strip()}")

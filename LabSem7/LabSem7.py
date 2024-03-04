c02c906457bac0af147e50947a1a4e51e997541c
José Riobueno <jrrv99@gmail.com>
2024-03-03 22:37:47 -0400

    feat: add union function with tests passed

--- LabSem7/LabSem7.py
+++ LabSem7/LabSem7.py
@@ -2,6 +2,7 @@ from typing import List
 
 SetType = List[int]
 
+
 def es_conjunto(lista: SetType) -> bool:
     items = []
     for item in lista:
@@ -9,3 +10,15 @@ def es_conjunto(lista: SetType) -> bool:
             items.append(item)
 
     return len(lista) == len(items)
+
+
+def union(set_1: SetType, set_2: SetType, swap: bool = False) -> SetType:
+    assert es_conjunto(set_1) and es_conjunto(set_2)
+
+    result = set_1 + [number for number in set_2 if number not in set_1]
+
+    if swap:
+        set_1.clear()
+        set_1.extend(result)
+
+    return result
--- LabSem7/tests.py
+++ LabSem7/tests.py
@@ -1,7 +1,7 @@
 from typing import List, Tuple
 import unittest
 
-from LabSem7 import es_conjunto
+from LabSem7 import es_conjunto, union
 
 
 class TestPrintMenu(unittest.TestCase):
@@ -10,6 +10,30 @@ class TestPrintMenu(unittest.TestCase):
 
         for value, expected_output in test_cases:
             self.assertEqual(es_conjunto(value), expected_output)
+
+    def tests_union(self):
+        test_cases = [([[0], [1], False], [0, 1]), ([[0], [2], True], [0, 2])]
+
+        for (a, b, swap), expected_output in test_cases:
+            if not swap:
+                self.assertEqual(union(a, b, swap), expected_output)
+                continue
+
+            result = union(a, b, swap)
+            self.assertEqual(result, expected_output)
+            self.assertEqual(a, result)
+
+        assertion_test_cases = [
+            ([1, 1], [2, 1]),
+            ([1, 2], [2, 2]),
+            ([1, 1], [2, 2]),
+            ([1, 1], [2, 1], True),
+            ([1, 0], [2, 2], True),
+            ([1, 1], [2, 2], True),
+        ]
+        for args in assertion_test_cases:
+            with self.assertRaises(AssertionError):
+                union(*args)
 
 
 if __name__ == "__main__":
